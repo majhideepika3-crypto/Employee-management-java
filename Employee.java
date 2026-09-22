cad8b1cfa8c785d34b66e1dfc812482d86d62a41
majhideepika3-crypto <majhideepika3@gmail.com>
2026-09-22 16:39:04 +0530

    Create Employee class with constructor and fields

---
+++ Employee.java
@@ -0,0 +1,7 @@
+public class Employee {
+    int id; String name; String email; String department; int salary;
+    public Employee(int id, String name, String email, String dept, int sal){
+        this.id=id; this.name=name; this.email=email; 
+        this.department=dept; this.salary=sal;
+    }
+}
